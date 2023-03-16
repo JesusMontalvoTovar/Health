69fc3f3b5212e0d603e8e68bad0557ce6fa93b26
jesus.montalvo.tovar <jesus.montalvo.tovar@accenture.com>
2023-03-16 12:41:43 -0600

    agregar Patient controller y Patient Service

---
+++ src/main/java/com/Health/Controller/PatientController.java
@@ -0,0 +1,66 @@
+package com.Health.Controller;
+
+import com.Health.Model.Patient;
+import com.Health.Service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class PatientController {
+
+    @Autowired
+    PatientService patientService;
+
+    @GetMapping("patients")
+    public ResponseEntity<List<Patient>> getAll() {
+
+        return new ResponseEntity<>(patientService.getAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("patients/{id}")
+    public ResponseEntity<Patient> getById(@PathVariable int id) {
+
+        Optional<Patient> patient = Optional.ofNullable(patientService.getById(id));
+        if (patient.isPresent()) {
+            return new ResponseEntity<>(patient.get(), HttpStatus.OK);
+        } else {
+            //throw new RecordNotFoundException();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping(path = "patients/new",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Patient> create(@RequestBody Patient newPatient) {
+
+        Patient patient = patientService.save(newPatient);
+        if (patient == null) {
+            //throw new ServerException();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } else {
+            return new ResponseEntity<>(patient, HttpStatus.CREATED);
+        }
+    }
+
+    @PutMapping(path = "patients/update",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Patient> update(@RequestBody Patient existingPatient) {
+
+        Patient patient = patientService.update(existingPatient);
+        if (patient == null) {
+            //throw new ServerException();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } else {
+            return new ResponseEntity<>(patient, HttpStatus.CREATED);
+        }
+    }
+
+}
---
+++ src/main/java/com/Health/Service/PatientService.java
@@ -0,0 +1,28 @@
+package com.Health.Service;
+
+import com.Health.Model.Patient;
+
+import java.util.List;
+
+public class PatientService {
+
+    public List<Patient> getAll()
+    {
+        return null;
+    }
+
+    public Patient getById(int id)
+    {
+        return null;
+    }
+
+    public Patient save(Patient newPatient)
+    {
+        return null;
+    }
+
+    public Patient update(Patient patient)
+    {
+        return null;
+    }
+}
